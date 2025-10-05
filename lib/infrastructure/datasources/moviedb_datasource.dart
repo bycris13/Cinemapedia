bd00c629a55f221694426f4aadcb7cca3f95599b
Cristian <cristianariasu13@gmail.com>
2025-10-05 12:41:47 -0500

    Provider Riverpod implementado con infrastructure

---
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,32 @@
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
+import 'package:dio/dio.dart';
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasource/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+class MoviedbDatasource extends MoviesDatasource {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3/',
+      queryParameters: {
+        'api_key': Environment.theMovieDbKey,
+        'language': 'es-MX',
+      },
+    ),
+  );
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('movie/now_playing');
+
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movie = movieDbResponse.results
+        .where((movieDb) => movieDb.posterPath != 'no-poster')
+        .map((movieDb) => MovieMapper.movieDBtoEntity(movieDb))
+        .toList();
+
+    return movie;
+  }
+}
---
+++ lib/infrastructure/repositories/movie_repository_implementation.dart
@@ -0,0 +1,14 @@
+import 'package:cinemapedia/domain/datasource/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repositories.dart';
+
+class MovieRepositoryImplementation extends MoviesRepositories {
+  final MoviesDatasource datasource;
+
+  MovieRepositoryImplementation(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+}
